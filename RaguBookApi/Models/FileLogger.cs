48458b8ea38face8676131c34e35a3b18ffb920a
Karuppaiah <rkaruppaiah@internal.tavisca.com>
2019-09-09 17:58:27 +0530

    added logger file

---
+++ RaguBookApi/Models/FileLogger.cs
@@ -0,0 +1,33 @@
+﻿using RaguBookApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RaguBookApi.Models
+{
+    public class FileLogger : ILog
+    {
+        private string filePath = @"C:\Users\rkaruppaiah\Desktop\RaguWS\WebApiWs\RaguBookApi\log.txt";
+        public void Log(string message)
+        {
+            if(File.Exists(filePath))
+            {
+                using (StreamWriter streamWriter = File.AppendText(filePath))
+                {
+                    streamWriter.WriteLine(message);
+                    streamWriter.Close();
+                }
+            }
+            else
+            {
+                using (StreamWriter streamWriter = new StreamWriter(filePath))
+                {
+                    streamWriter.WriteLine(message);
+                    streamWriter.Close();
+                }
+            }
+        }
+    }
+}
--- RaguBookApi/Services/BookService.cs
+++ RaguBookApi/Services/BookService.cs
@@ -3,6 +3,7 @@ using RaguBookApi.Models;
 using RaguBookApi.Validators;
 using System.Collections.Generic;
 using FluentValidation.Results;
+using System;
 
 namespace RaguBookApi.Services
 {
@@ -12,21 +13,25 @@ namespace RaguBookApi.Services
         BookValidator validate = new BookValidator();
         ValidationResult result;
         List<Error> errors = new List<Error>();
+        ILog logger = new FileLogger();
         public Response Delete(int id)
         {
             if(Validate.IsIdPositiveNumber(id))
             {
                 if(libray.Remove(id))
                 {
+                    logger.Log("Deleted : " + "Successfully" + " " + DateTime.Now);
                     return new Response(new Book(), null);
                 }
                 else
                 {
+                    logger.Log("Error Occured while Deleting a Book : IdNotFound " + DateTime.Now);
                     return new Response(null, new Error("Id", "IdNotFound"));
                 }
             }
             else
             {
+                logger.Log("Error Occured while Deleting a Book : InValid Id Should Be Positive " + DateTime.Now);
                 return new Response(null, new Error("Id", "Invalid Id, Id should be a positive number"));
             }
         }
@@ -42,16 +47,19 @@ namespace RaguBookApi.Services
             {
                 if(libray.Get(id) != null)
                 {
+                    logger.Log("Fetched : Successfully " + DateTime.Now);
                     return new Response(libray.Get(id), null);
                 }
                 else
                 {
+                    logger.Log("Error Occured while Geting Book : IdNotFound " + DateTime.Now);
                     return new Response(null, new Error("Id", "IdNotFound"));
                 }
                 
             }
             else
             {
+                logger.Log("Error Occured while Getting Book : InValid Id Should Be Positive " + DateTime.Now);
                 return new Response(null, new Error("Id", "Invalid Id, Id should be a positive number"));
             }
         }
@@ -62,6 +70,7 @@ namespace RaguBookApi.Services
             if(result.IsValid)
             {
                 libray.Add(book);
+                logger.Log("Created : "+book.ToString() + " Added Successfully" + " " + DateTime.Now);
                 return new Response(book, null);
             }
             else
@@ -70,6 +79,7 @@ namespace RaguBookApi.Services
                 {
                     errors.Add(new Error(error.PropertyName, error.ErrorMessage));
                 }
+                logger.Log("Error Occured while Creating Book : " + errors[0].message+" "+ DateTime.Now);
                 return new Response(null, errors[0]);
                 
             }
@@ -84,10 +94,12 @@ namespace RaguBookApi.Services
                 {
                     if (libray.Update(id, book))
                     {
+                        logger.Log("Updated :"+book.ToString() + " Updated Successfully");
                         return new Response(book, null);
                     }
                     else
                     {
+                        logger.Log("Error in Updating Book : IdNotFound " + DateTime.Now);
                         return new Response(null, new Error("Id", "IdNotFound"));
                     }
                 }
@@ -97,11 +109,13 @@ namespace RaguBookApi.Services
                     {
                         errors.Add(new Error(error.PropertyName, error.ErrorMessage));
                     }
+                    logger.Log("Error Occured while Updating Book : " + errors[0].message + " " + DateTime.Now);
                     return new Response(null, errors[0]);
                 }
             }
             else
             {
+                logger.Log("Error in Updating Book : InValid Id Should Be Positive " + DateTime.Now);
                 return new Response(null, new Error("Id", "Invalid Id, Id should be a positive number"));
             }
         }
